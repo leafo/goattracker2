bd64ceb7e68a51ce198c5955952ebf664c5312dd
Leandro Nini <drfiemost@users.sourceforge.net>
2021-02-19 09:06:52 +0100

    Fix cmake warning

--- cmake/FindExSID.cmake
+++ cmake/FindExSID.cmake
@@ -7,7 +7,7 @@ find_path(EXSID_INCLUDE_DIR exSID.h HINTS ${EXSID_INCLUDEDIR} ${EXSID_INCLUDE_DI
 find_library(EXSID_LIBRARY NAMES exsid HINTS ${EXSID_LIBDIR} ${EXSID_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(exsid DEFAULT_MSG EXSID_LIBRARY EXSID_INCLUDE_DIR)
+find_package_handle_standard_args(ExSID DEFAULT_MSG EXSID_LIBRARY EXSID_INCLUDE_DIR)
 
 mark_as_advanced(EXSID_INCLUDE_DIR EXSID_LIBRARY)
 
